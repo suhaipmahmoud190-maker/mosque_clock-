b3549100292105e3375e3dee251687fa7c7a41c1
suhaipmahmoud190-maker <suhaipmahmoud190@gmail.com>
2026-06-20 10:27:59 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'mosque-clock-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html'
+];
+
+// تخزين الملفات عند أول تثبيت
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// تفعيل وتنظيف أي نسخ قديمة
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// تقديم النسخة المخزنة أولاً، وإذا ما لقاها يحاول الانترنت
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((response) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      }).catch(() => cached);
+    })
+  );
+});
